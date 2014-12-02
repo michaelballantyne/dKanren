38e9b41c1c7718d1aa6bd95321ab7ce7ba497614
Michael Ballantyne <michael.ballantyne@gmail.com>
2014-12-02 13:26:35 -0700

    Separate out constraint application.

--- fairconj.rkt
+++ fairconj.rkt
@@ -6,36 +6,56 @@
 
 (provide == run run* fresh conde)
 
-(define (empty-s)
-  '())
 
-(struct conj (substitution unsorted disjunctions) #:transparent)
+; Constraints
+
+(define (empty-constraint-store)
+  '(() () () () () () ()))
+
+(struct unification (term1 term2) #:transparent)
+
+(define (== term1 term2)
+  (constraint (unification term1 term2)))
+
+
+(define (apply-constraint constraint constraint-store)
+  (match-define (list a b substitution c d e f) constraint-store)
+  (match constraint
+    [(unification term1 term2)
+     (let ([new-substitution (unify term1 term2 substitution)])
+       (and new-substitution (list a b new-substitution c d e f)))]
+    ))
+
+
+; Search
+
+(struct conj (constraint-store unsorted disjunctions) #:transparent)
 (struct disj (children) #:transparent)
-(struct unification (left right) #:transparent)
+(struct constraint (c) #:transparent)
 (struct state (unreduced reduced) #:transparent)
 
-(define (reduce-conj substitution unsorted disjunctions)
+(define (reduce-conj constraint-store unsorted disjunctions)
   (if (null? unsorted)
-    (values substitution disjunctions)
+    (values constraint-store disjunctions)
     (match (first unsorted)
-      [(unification term1 term2)
-       (let ([new-substitution (unify term1 term2 substitution)])
+      [(constraint c)
+       (let ([new-constraint-store (apply-constraint c constraint-store)])
          (cond
-           [(not new-substitution)
+           [(not new-constraint-store)
             (values #f #f)]
            [(null? (rest unsorted))
-            (values new-substitution disjunctions)]
-           [else (reduce-conj new-substitution (rest unsorted) disjunctions)]))]
+            (values new-constraint-store disjunctions)]
+           [else (reduce-conj new-constraint-store (rest unsorted) disjunctions)]))]
       [(conj #f nested-unsorted #f)
-       (let-values ([(new-substitution new-disjunctions)
-                     (reduce-conj substitution nested-unsorted disjunctions)])
-         (if new-substitution
-           (reduce-conj new-substitution (rest unsorted) new-disjunctions)
+       (let-values ([(new-constraint-store new-disjunctions)
+                     (reduce-conj constraint-store nested-unsorted disjunctions)])
+         (if new-constraint-store
+           (reduce-conj new-constraint-store (rest unsorted) new-disjunctions)
            (values #f #f)))]
       ; Only disjunctions are inverse-eta delayed, so evaluate the procedure
       ; and enqueue the resulting disjunction.
       [(and p (? procedure?))
-       (reduce-conj substitution
+       (reduce-conj constraint-store
                     (rest unsorted)
                     (and disjunctions
                          (disjq:enqueue
@@ -43,70 +63,84 @@
                            disjunctions)))]
       )))
 
-(define (distribute into-disjunction other-disjunctions substitution)
+(define (distribute into-disjunction other-disjunctions constraint-store)
   (map
     (match-lambda
       [(conj #f nested-unsorted #f)
-       (conj substitution nested-unsorted other-disjunctions)])
+       (conj constraint-store nested-unsorted other-disjunctions)])
     (disj-children into-disjunction)))
 
-(define (filter-branches disjunction substitution)
+(define (filter-branches disjunction constraint-store)
   (define new-children
     (filter-map
       (match-lambda
         [(conj #f children #f)
-         (define-values (new-substitution new-disjunctions)
-           (reduce-conj substitution children #f))
-         (and new-substitution
+         (define-values (new-constraint-store new-disjunctions)
+           (reduce-conj constraint-store children #f))
+         (and new-constraint-store
               (conj #f children #f))])
       (disj-children disjunction)))
   (and (pair? new-children) (disj new-children)))
 
-(define (filter-disjunctions remaining substitution acc)
+(define (filter-disjunctions remaining constraint-store acc)
   (if (disjq:empty? remaining)
     acc
     (let ([filtered (filter-branches (disjq:head remaining)
-                                     substitution)])
+                                     constraint-store)])
       (and
         filtered
         (filter-disjunctions (disjq:tail remaining)
-                             substitution
+                             constraint-store
                              (disjq:enqueue filtered acc))))))
 
 (define (step-state current-state)
   (match-define (state unreduced reduced) current-state)
   (if (pair? unreduced)
-    (match-let ([(conj substitution unsorted disjunctions)
+    (match-let ([(conj constraint-store unsorted disjunctions)
                  (first unreduced)])
-      (define-values (new-substitution new-disjunctions)
-        (reduce-conj substitution unsorted (disjq:queue)))
+      (define-values (new-constraint-store new-disjunctions)
+        (reduce-conj constraint-store unsorted (disjq:queue)))
       (define filtered-disjunctions
         (and new-disjunctions
-             (filter-disjunctions new-disjunctions new-substitution disjunctions)))
+             (filter-disjunctions new-disjunctions new-constraint-store disjunctions)))
       (cond
         [(not filtered-disjunctions)
          (values #f (state (rest unreduced) reduced))]
         [(disjq:empty? filtered-disjunctions)
-         (values new-substitution (state (rest unreduced) reduced))]
+         (values new-constraint-store (state (rest unreduced) reduced))]
         [else
          (values #f
                  (state (rest unreduced)
-                        (topq:enqueue (conj new-substitution '() filtered-disjunctions)
+                        (topq:enqueue (conj new-constraint-store '() filtered-disjunctions)
                                       reduced)))]))
-    (match-let ([(conj substitution '() disjunctions)
+    (match-let ([(conj constraint-store '() disjunctions)
                  (topq:head reduced)])
       (values #f (state
                    (distribute (disjq:head disjunctions)
                                (disjq:tail disjunctions)
-                               substitution)
+                               constraint-store)
                    (topq:tail reduced))))))
 
-
-(define (substitution->constraint-store substitution)
-  `(() () ,substitution () () () ()))
-
 (define (inject . conjuncts)
-  (state (list (conj (empty-s) conjuncts (disjq:queue))) (topq:queue)))
+  (state (list (conj (empty-constraint-store) conjuncts (disjq:queue))) (topq:queue)))
+
+(define (run-internal n query-vars current-state acc)
+  (if (and (or (pair? (state-unreduced current-state))
+               (not (topq:empty? (state-reduced current-state))))
+           (or (false? n) (> n 0)))
+    (let-values ([(result new-state) (step-state current-state)])
+      (if result
+        (run-internal (and n (- n 1)) query-vars new-state (cons result acc))
+        (run-internal n query-vars new-state acc)))
+    (map (lambda (result)
+           ((reify (if (= (length query-vars) 1)
+                     (car query-vars)
+                     query-vars))
+            result))
+         (reverse acc))))
+
+
+; Interface macros
 
 (define-syntax (fresh stx)
   (syntax-case stx ()
@@ -124,23 +158,7 @@
              (conj #f (list g g* ...) #f)
              ...)))]))
 
-(define (== a b)
-  (unification a b))
 
-(define (run-internal n query-vars current-state acc)
-  (if (and (or (pair? (state-unreduced current-state))
-               (not (topq:empty? (state-reduced current-state))))
-           (or (false? n) (> n 0)))
-    (let-values ([(result new-state) (step-state current-state)])
-      (if result
-        (run-internal (and n (- n 1)) query-vars new-state (cons result acc))
-        (run-internal n query-vars new-state acc)))
-    (map (lambda (result)
-           ((reify (if (= (length query-vars) 1)
-                     (car query-vars)
-                     query-vars))
-            (substitution->constraint-store result)))
-         (reverse acc))))
 
 (define-syntax (run stx)
   (syntax-case stx ()
@@ -151,15 +169,15 @@
          (run-internal n
                        (list x x* ...)
                        (inject g g* ...)
-                       '()))
-     ]))
+                       '()))]))
 
 (define-syntax (run* stx)
   (syntax-case stx ()
     [(_ (x x* ...) g g* ...)
      #'(run #f (x x* ...) g g* ...)]))
 
-;; Debugging tools
+
+; Debugging tools
 
 (define (state-display s)
   (list (state-unreduced s) (topq:queue->list (state-reduced s))))
